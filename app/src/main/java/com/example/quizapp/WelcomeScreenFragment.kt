8e8dce4232bd4bafbc6fe466351109b2aa65d085
BGBorisova <borislavagb@gmail.com>
2022-10-21 20:32:27 +0300

    change default screen names

---
+++ app/src/main/java/com/example/quizapp/WelcomeScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.example.quizapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.quizapp.databinding.FragmentWelcomeScreenBinding
+
+class WelcomeScreenFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentWelcomeScreenBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWelcomeScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
